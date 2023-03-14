2ce684d1b256aa0afd374f51a27f46bdb607219c
InhooPark <park.linhoo@gmail.com>
2023-03-14 11:54:39 +0900

    back

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -38,7 +38,7 @@ export default NextAuth({
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET,
   callbacks: {
     async jwt({ token }) {
       return token;
